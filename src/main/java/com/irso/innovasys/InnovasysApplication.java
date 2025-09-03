711c42c47cbc5e7dc2725b6f82307696d160f50e
Penalver-Ezequiel <ezepenalver29@gmail.com>
2025-09-03 15:30:27 -0300

    Commit Inicial: Proyecto InnovaSys

---
+++ src/main/java/com/irso/innovasys/InnovasysApplication.java
@@ -0,0 +1,13 @@
+package com.irso.innovasys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovasysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovasysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irso/innovasys/InnovasysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irso.innovasys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovasysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
